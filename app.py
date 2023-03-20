4646204e38ba13a5ffdfa101baa9d2f3900bd3af
fmarsha <fmarsha@student.21-school.ru>
2023-03-20 23:47:45 +0300

    First api

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/get_mem")
+def mem():
+    return "<p>Заходят значит 3 еврея в бар...</p>"
