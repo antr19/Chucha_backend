0fd7b368a99d4e2a3546a42198ed8091174c8f8d
fmarsha <fmarsha@student.21-school.ru>
2023-03-23 22:33:42 +0300

    Add database

---
+++ db.py
@@ -0,0 +1,3 @@
+from sqlalchemy import *
+
+engine = create_engine("postgresql+psycopg2://svc_database:XLQVRjpM5ktMt9yOkBq2vKqxRFKYgBIj@dpg-cgdkp01mbg54astb1drg-a.oregon-postgres.render.com/main_s2ox")
