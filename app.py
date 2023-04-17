5ea5062143d87d8eb3859f53ecdaff70ce1c9de1
fmarsha <fmarsha@student.21-school.ru>
2023-04-17 23:08:00 +0300

    Add swagger

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
+
 import models.users
+from auth.auth import auth, test_print
+from swagger.swagger import swaggerui_blueprint
 
 
 def create_app():
@@ -9,9 +12,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
     # blueprint for auth routes in our app
-    from auth.auth import auth, test_print
     test_print()
     app.register_blueprint(auth)
+    app.register_blueprint(swaggerui_blueprint)
 
     return app
 
---
+++ swagger/swagger.py
@@ -0,0 +1,23 @@
+from flask_swagger_ui import get_swaggerui_blueprint
+from conf import *
+
+
+SWAGGER_URL = "/".join(["", "api", VERSION, "docs"])  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Chucha's backend api"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
